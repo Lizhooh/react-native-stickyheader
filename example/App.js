56192acc9b10757fd8812ffda3f4fae6bf95b3fa
lizhooh <960316408@qq.com>
2019-08-29 11:07:14 +0800

    update: 更新一波。

--- example/App.js
+++ example/App.js
@@ -6,79 +6,112 @@
 
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  Dimensions,
-  SectionList,
-  Animated,
-  ScrollView
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    Dimensions,
+    SectionList,
+    Animated,
+    ScrollView,
+    RefreshControl,
 } from 'react-native';
 
 
 import StickyHeader from 'react-native-stickyheader';
 
-
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { scrollY: new Animated.Value(0) }
-    this.data = []
-    for (let index = 0; index < 100; index++) {
-      this.data.push(index)
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            scrollY: new Animated.Value(0),
+            loadMore: false,
+            list: [...new Array(20)].map((v, i) => i),
+        };
     }
-  }
 
-  _renderItem = (info) => {
-    return (
-      <View
-        style={{ height: 50, backgroundColor: '#ffffff' }}>
-        <Text>{info.item}</Text>
-      </View>
+    renderItem = (item, index) => (
+        <View
+            key={index}
+            style={{
+                height: 50,
+                backgroundColor: '#fff',
+                borderBottomColor: '#ccc',
+                borderBottomWidth: 0.3
+            }}>
+            <Text style={{ textAlign: 'center' }}>{index}</Text>
+        </View>
     )
-  }
-  _keyExtractor = (item, index) => {
-    return index
-  }
 
-  render() {
+    onScroll = e => {
+        if (this.state.loadMore) return;
+        if (this.state.list.length > 200) {
+            this.setState({ loadMore: false });
+            return;
+        }
 
-    return (
-      <View style={styles.container}>
-        <View style={{ height: 64, backgroundColor: '#973444' }}></View>
-        <Animated.ScrollView
-          onScroll={Animated.event([{
-            nativeEvent: { contentOffset: { y: this.state.scrollY, } }
-          }], { useNativeDriver: true })}
-          scrollEventThrottle={1}
-        >
-          <Text>文字</Text>
-          <Text>文字</Text>
-          <Text>文字</Text>
-          <Text>文字</Text>
-          <StickyHeader
-            stickyHeaderY={60} // 滑动到多少悬浮
-            stickyScrollY={this.state.scrollY}
-          >
-            <View style={{ height: 60, backgroundColor: 'red' }} />
-          </StickyHeader>
-          <FlatList
-            ref={flatList => this._flatList = flatList}
-            data={this.data}
-            keyExtractor={this._keyExtractor}
-            renderItem={this._renderItem}
-          />
-        </Animated.ScrollView>
-      </View>
-    );
-  }
+        const data = e.nativeEvent;
+        const y = data.contentOffset.y;
+        const height = data.layoutMeasurement.height;
+        const contentHeight = data.contentSize.height;
+
+        // 滚动到底部，触发加载更多
+        if (y + height >= contentHeight - 20) {
+            this.setState({ loadMore: true });
+            setTimeout(() => {
+                this.setState({
+                    loadMore: false,
+                    list: [...this.state.list, ...this.state.list],
+                });
+            }, 1000 * 2);
+        }
+    }
+
+    render() {
+        const { scrollY, loadMore, list = [] } = this.state;
+
+        return (
+            <View style={styles.container}>
+                <View style={{ height: 64, backgroundColor: '#f6f6f6' }}></View>
+                <Animated.ScrollView
+                    scrollEventThrottle={1}
+                    refreshControl={<RefreshControl refreshing={false} />}
+                    onScroll={
+                        Animated.event(
+                            [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                            { useNativeDriver: true, listener: this.onScroll }
+                        )
+                    }>
+                    <Text>文字</Text>
+                    <Text>文字</Text>
+                    <Text>文字</Text>
+                    <Text>文字</Text>
+
+                    <StickyHeader
+                        stickyHeaderY={60} // 滑动到多少悬浮
+                        stickyScrollY={scrollY}>
+                        <View style={{ height: 60, backgroundColor: '#39f' }} />
+                    </StickyHeader>
+
+                    {list.map(this.renderItem)}
+
+                    {loadMore &&
+                        <View style={{ backgroundColor: '#39f', height: 50 }}>
+                            <Text>加载更多</Text>
+                        </View>
+                    }
+
+                </Animated.ScrollView>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        justifyContent: 'center',
+    },
 });
